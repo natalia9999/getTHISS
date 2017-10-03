f5c29fec8ec8d30c1bead8d12144f03f46d98e6f
natalia9999 <32482466+natalia9999@users.noreply.github.com>
2017-10-03 13:20:44 +0200

    Add files via upload

---
+++ Esercizio_post.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<title> Registrazione utente </title>
+</head>
+<body>
+<h2> Modulo di registrazione </h2>
+<hr/ >
+Inserisci i dati e spedisci il modulo
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>"method="post">
+Nome <input type="text" name="nome" size="20">
+<br />
+Cognome <input type="text" name="cognome" size="20">
+<br />
+E-mail <input type="text" name="email" size="30">
+<br />
+<input type="submit" name="invio" value="Registra" />
+<input type="reset" name="cancella" value="Anulla" />
+</form>
+<hr />
+
+<?php if(isset($_POST["invio"])) {
+$nome=$_POST["nome"];
+$cognome=$_POST["cognome"];
+$email=$_POST["email"];
+echo  "Conferma della registrazione: <br />";
+echo  "<br />";
+echo "La richiesta di $nome $cognome è stata registrata con l'indirizzo di posta elettronica $email. <br />";
+
+echo "La richiesta di accesso al servizio è arrivata alle ore ", date ("H:i:s"), "del giorno: ", date("d-m-Y");
+echo " <br />";
+
+}
+?>
+</body>
+</html>
